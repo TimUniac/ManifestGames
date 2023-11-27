0b6128afe761eada7290187c9b662e4f69f87a3d
Stephilles <149612545+Stephilles@users.noreply.github.com>
2023-11-26 21:42:43 -0500

    Nov. 26 Everything works again

---
+++ Tims present/script/GameTimer Tutorial.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var startTime = 60
+var seconds = startTime
+var minutes = 1
+
+
+func _on_timer_timeout():
+	game_time()
+	
+func game_time():
+	if seconds > 0:
+		seconds -= 1
+		$Label.text = str(minutes) + ":" + str(seconds)	
+	else:
+		seconds = startTime - 1 
+		if minutes > 0:
+			minutes -= 1
+			$Label.text = str(minutes) + ":" +str(seconds)
+		else:
+			$Timer.stop()
+			get_tree().change_scene_to_file("res://Scenes/male_office.tscn")
+	#change to chapter 2 when that sceen is created
+
+
+
