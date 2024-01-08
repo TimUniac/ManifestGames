34c0ba14736b46a8b63ae3083029e354b9d4f9f7
TimUniac <70977289+TimUniac@users.noreply.github.com>
2024-01-08 00:53:53 -0500

    More Dialogue work

--- Tims present/Scenes/Levels/dialogue.gd
+++ Tims present/Scenes/Levels/dialogue.gd
@@ -1,42 +1,34 @@
 extends TextureRect
 
-@export var textSpeed: float = 0.05
-
-var dialogue: Array
-var phraseNum = 0
-var finished = false
+var json= JSON.new()
+var json_data = []
+var current_dialogue_index = 0
+var file = FileAccess.open("res://Assets/TemplateDialogue.json",FileAccess.READ)
 
 func _ready():
-	$Timer.wait_time = textSpeed
-	nextPhrase()
+	
+	if file:
+		var content = file.get_as_text()
+		file.close()  # Close the file after reading its content
 
-func getDialogue() -> Array:
-	var file = FileAccess.open("res://Assets/TemplateDialogue.json", FileAccess.READ)
-	if FileAccess.open("res://Assets/TemplateDialogue.json", FileAccess.READ):
-		print("Failed to open file.")
-	return []
-
-	var json_data = file.get_as_text()
-	file.close()
-
-	var json = JSON.new()
-	var result = json.parse(json_data)
-	if result.error == OK:
-		return result.result
+		# JSON parsing
+		var json_result = json.parse(content)
+	
 	else:
-		print("Failed to parse JSON: ", result.error)
-		return []
-
-func nextPhrase() -> void:
-	if phraseNum >= dialogue.size():
-		queue_free()
-		return
-
-	$Name.bbcode_text = dialogue[phraseNum]["Name"]
-	$Text.bbcode_text = dialogue[phraseNum]["Text"]
-	$Text.visible_characters = 0
-	phraseNum += 1
-
-	while $Text.visible_characters < $Text.bbcode_text.length():
-		$Text.visible_characters += 1
-		await $Timer.timeout
+		print("Failed to open file")
+		
+		
+func update_dialogue(dialogue_data):
+	$Name.text = dialogue_data["characterName"]
+	# $Portrait.texture = load(dialogue_data["portrait"])
+	$Text.text = dialogue_data["dialogue"]
+	
+func _input(event):
+	if event.is_action_pressed("Pickup") and current_dialogue_index < json_data.size() - 1:
+		# 'E' key pressed, go to next dialogue
+		current_dialogue_index += 1
+		update_dialogue(file)
+	elif event.is_action_pressed("up") and current_dialogue_index > 0:
+		# 'W' key pressed, go to previous dialogue
+		current_dialogue_index -= 1
+		update_dialogue(file)
