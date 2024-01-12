eac3bd561b3a4271742cb07b93e1fd6b1e442da1
TimUniac <timuniac@gmail.com>
2024-01-12 12:16:08 -0500

    Dialogue BBcode start

--- Tims present/Scenes/Levels/dialogue.gd
+++ Tims present/Scenes/Levels/dialogue.gd
@@ -1,34 +1,13 @@
 extends TextureRect
 
-var json= JSON.new()
-var json_data = []
-var current_dialogue_index = 0
-var file = FileAccess.open("res://Assets/TemplateDialogue.json",FileAccess.READ)
-
-func _ready():
+func _process(delta):
+	$Text
+	if (Input.is_action_pressed("Interact")):
+		docTalk()
+	if (Input.is_action_pressed("up")):
+		seraTalk()
+func docTalk():
+	$Name.append_bbcode("[b][font=Noto Sans][color=gray][size=7]Doctor[/size][/color][/font][/b]")
 	
-	if file:
-		var content = file.get_as_text()
-		file.close()  # Close the file after reading its content
-
-		# JSON parsing
-		var json_result = json.parse(content)
-	
-	else:
-		print("Failed to open file")
-		
-		
-func update_dialogue(dialogue_data):
-	$Name.text = dialogue_data["characterName"]
-	# $Portrait.texture = load(dialogue_data["portrait"])
-	$Text.text = dialogue_data["dialogue"]
-	
-func _input(event):
-	if event.is_action_pressed("Pickup") and current_dialogue_index < json_data.size() - 1:
-		# 'E' key pressed, go to next dialogue
-		current_dialogue_index += 1
-		update_dialogue(file)
-	elif event.is_action_pressed("up") and current_dialogue_index > 0:
-		# 'W' key pressed, go to previous dialogue
-		current_dialogue_index -= 1
-		update_dialogue(file)
+func seraTalk():
+	$Name.append_bbcode("[b][font=Noto Sans][color=gray][size=7]Seraphina[/size][/color][/font][/b]")
