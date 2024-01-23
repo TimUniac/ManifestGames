aa607ebdc67f257fcd0d737ef23c8969e3490d40
Stephilles <149612545+Stephilles@users.noreply.github.com>
2024-01-23 17:50:01 -0500

    Settings and in game settings

---
+++ Tims present/Scenes/Levels/bedroomsetting.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+
+func _ready():
+	$CanvasLayer2/SettingsPanel.visible = false
+	$"Settings toggle".visible = true
+
+
+var opened = false
+
+func close():
+	$CanvasLayer2/SettingsPanel.visible = false
+	opened = false
+func open():
+	$CanvasLayer2/SettingsPanel.visible = true
+	opened = true
+
+		
+func _on_settings_toggle_pressed():
+	if opened:
+		close()
+		
+	else:
+		open()
+		print("hey")
+
+
