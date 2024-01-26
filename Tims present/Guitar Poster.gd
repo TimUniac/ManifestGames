fee8176914325c1a204513da5503d52d7071bcd8
TimUniac <timuniac@gmail.com>
2024-01-26 13:10:11 -0500

    Guitar Game Mostly done.
    Poster not working. Needs sounds

--- Tims present/Guitar Poster.gd
+++ Tims present/Guitar Poster.gd
@@ -3,13 +3,14 @@ extends Control
 var is_opened = false
 
 func _ready():
-	$Orbs.visible = false
+	$ChordChart.visible = false
 	
 func _on_interaction_area_body_entered(body):
 	if Input.is_action_just_pressed("Interact"):
 		if is_opened:
-			$Orbs.visible = false
+			$ChordChart.visible = false
+			print ("Look")
 		else:
-			$Orbs.visible = true
+			$ChordChart.visible = true
 		
 	
