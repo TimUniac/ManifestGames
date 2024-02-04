4dd88b84c2bddc1effebd1a607b7ceb828772f34
TimUniac <70977289+TimUniac@users.noreply.github.com>
2024-02-03 23:47:30 -0500

    Office R fixed

--- Tims present/Scenes/Levels/male_office.gd
+++ Tims present/Scenes/Levels/male_office.gd
@@ -5,12 +5,12 @@ extends Node2D
 func _ready():
 	audio_stream_player.playing = true
 	
-func _process(delta):
-	change_scene()
+#func _process(delta):
+#	change_scene()
 
-func change_scene():
-	if Input.is_action_just_pressed("Next"):
-		get_tree().change_scene_to_file("res://Scenes/Levels/seraphina_bedroom_male.tscn")
+#func change_scene():
+#	if Input.is_action_just_pressed("Next"):
+#		get_tree().change_scene_to_file("res://Scenes/Levels/seraphina_bedroom_male.tscn")
 
 
 
