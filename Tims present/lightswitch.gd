bcde3355e70494800d3a116c1442fd2cbacfec56
TimUniac <timuniac@gmail.com>
2024-03-03 17:33:22 -0500

    lightswitch.gd stuff

--- Tims present/lightswitch.gd
+++ Tims present/lightswitch.gd
@@ -17,7 +17,7 @@ func _ready():
 func _pressed():
 	switcher()
 	emit_signal("switch")
-		
+	switch_neighbors()
 func switcher():
 	if ON == false:
 		self.texture_normal = switchON
@@ -27,5 +27,27 @@ func switcher():
 		self.texture_normal = switchOFF
 		ON = false
 		root.lightOff()
+func switch_neighbors():
+	var ids_to_switch = []
+	var grid_width = 5
+	
+# Calculate the neighbors
+	var up = button_id - grid_width if button_id > grid_width else 0
+	var down = button_id + grid_width if button_id <= (grid_width * (grid_width - 1)) else 0
+	var left = button_id - 1 if button_id % grid_width != 1 else 0
+	var right = button_id + 1 if button_id % grid_width != 0 else 0
+
+	ids_to_switch.append(up) if up else 0
+	ids_to_switch.append(down) if down else 0
+	ids_to_switch.append(left) if left else 0
+	ids_to_switch.append(right) if right else 0
+
+	# Switch the state of each neighbor
+	for id in ids_to_switch:
+		if id == 0:
+			continue
+		var neighbor = root.get_node("switch_" + str(id))
+		if neighbor:
+			neighbor.switcher()
 
 
