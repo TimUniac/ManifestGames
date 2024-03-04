581a99d4f2d53f6962ffb3dbabc41ff204bfbd7a
Stephilles <149612545+Stephilles@users.noreply.github.com>
2024-03-04 17:20:43 -0500

    settings added to hallway and fixed movement while settings is open

--- Tims present/Scenes/Nodes/UI/in_game_settings.gd
+++ Tims present/Scenes/Nodes/UI/in_game_settings.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var doctor = get_parent().get_node("Male Doctor Character")
 
 func _ready():
 	$Camera2D/CanvasLayer2/SettingsPanel.visible = false
@@ -14,11 +14,13 @@ func close():
 	$Camera2D/CanvasLayer2/SettingsPanel.visible = false
 	opened = false
 	$"Settings toggle".visible = true
+	doctor.talking = false
 	
 func open():
 	$Camera2D/CanvasLayer2/SettingsPanel.visible = true
 	opened = true
 	$"Settings toggle".visible = false
+	doctor.talking = true
 
 		
 func _on_settings_toggle_pressed():
