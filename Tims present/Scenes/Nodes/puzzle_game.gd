7f1a13a697e2f11007d9aa41baa9d607f1605f8c
TimUniac <70977289+TimUniac@users.noreply.github.com>
2024-03-05 01:44:41 -0500

    Puzzle game done... needs proper ticket imgae

--- Tims present/Scenes/Nodes/puzzle_game.gd
+++ Tims present/Scenes/Nodes/puzzle_game.gd
@@ -1,15 +1,19 @@
 extends Node2D
-
-
-var total_pieces = 1
+@onready var answer = $Control/TextureRect
+@onready var blocker = $Control/ColorRect2
+var total_pieces = 9
 var pieces_snapped = 0
 
+func _ready():
+	answer.visible = false
+	
 func snapped_pieces():
 	pieces_snapped += 1
 	print(pieces_snapped)
 	if pieces_snapped == total_pieces:
 		winstate()
+		blocker.visible = false
+		answer.visible = true
 
 func winstate():
 	print("All pieces correctly placed!")
-	# Implement your win state logic here
