78cd98dae6a29b182b576d8ada81c492c96053cc
TimUniac <70977289+TimUniac@users.noreply.github.com>
2024-03-09 10:06:31 -0500

    Postit fixed

--- Tims present/postit.gd
+++ Tims present/postit.gd
@@ -1,21 +1,21 @@
 extends Control
 
 var is_opened = false
-@onready var areaLabel = $"Interaction Area"
+@onready var areaLabel = $"Interaction Area/Panel"
 @onready var in_range = false
 @onready var doctor = get_parent().get_node("Male Doctor Character")
 
 func _ready():
 	$BigPostit.visible = false
-	
+	areaLabel.visible = false
 func _process(_delta):
-	if areaLabel.interaction_label.visible == true:
+	if areaLabel.visible == true:
 		in_range = true
-	if areaLabel.interaction_label.visible == false:
+	if areaLabel.visible == false:
 		in_range = false
 	
 			
-	if Input.is_action_just_pressed("Look") and in_range:
+	if Input.is_action_just_pressed("Interact") and in_range:
 		if is_opened:
 			close()
 		else:
@@ -37,3 +37,11 @@ func open():
 
 func _on_close_pressed():
 	close()
+
+
+func _on_interaction_area_body_entered(body):
+	areaLabel.visible = true
+
+
+func _on_interaction_area_body_exited(body):
+	areaLabel.visible = false
