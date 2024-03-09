421b73aa58793f5571ac266138ce32fab4416faf
TimUniac <70977289+TimUniac@users.noreply.github.com>
2024-03-09 09:38:16 -0500

    Puzzle game polished up

--- Tims present/Scenes/Nodes/puzzle_game.gd
+++ Tims present/Scenes/Nodes/puzzle_game.gd
@@ -3,10 +3,11 @@ extends Node2D
 @onready var blocker = $Control/ColorRect2
 var total_pieces = 8 
 var pieces_snapped = 0
+@onready var root = $"."
 
 func _ready():
 	answer.visible = false
-	
+	root.visible = true
 func snapped_pieces():
 	pieces_snapped += 1
 	print(pieces_snapped)
@@ -31,4 +32,4 @@ func winstate():
 
 
 func _on_close_pressed():
-	$".".visible = false
+	root.visible = false
