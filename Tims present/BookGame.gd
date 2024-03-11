a4916537926fa72aa0535c90d36632e20c80cab3
Stephilles <149612545+Stephilles@users.noreply.github.com>
2024-03-10 23:10:21 -0400

    we blessed

--- Tims present/BookGame.gd
+++ Tims present/BookGame.gd
@@ -5,20 +5,19 @@ var typed_code = ""
 var entered_code = ""
 var sequence = 0
 
-func _process(_delta): #checks if entered code matches the secret code
+func _process(delta): #checks if entered code matches the secret code
 	if entered_code == secret_code:
-		print("Book Correct!")
+		print("Code Correct!")
 		typed_code = ""
 		entered_code = ""
 		var parent = get_parent()
 		if parent and parent.has_method("playercollect"):
 			parent.playercollect()
 			parent.close()
-			
-		else:
-			print ("No such method")
 
 
+func _ready():
+	pass
 	
 
 
@@ -28,6 +27,9 @@ func _append_letter_to_code(typed_code):
 	$CanvasLayer/SequenceInput.text = entered_code
 
 
+func wait(seconds: float, letter) -> void:
+	await get_tree().create_timer(seconds).timeout
+	letter.visible = true
 
 func _on_letter_c_button_down():
 	if sequence == 0:
@@ -35,38 +37,50 @@ func _on_letter_c_button_down():
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_C".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_C")
 func _on_letter_a_button_down():
 	if sequence == 1 or sequence == 7:
 		typed_code += "A"
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_A".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_A")
 func _on_letter_r_button_down():
 	if sequence == 2:
 		typed_code += "R"
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_R".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_R")
 func _on_letter_m_button_down():
 	if sequence == 3:
 		typed_code += "M"
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_M".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_M")
 func _on_letter_i_button_down():
 	if sequence == 4:
 		typed_code += "I"
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_I".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_I")
 func _on_letter_l_button_down():
 	if sequence == 5 or sequence == 6:
 		typed_code += "L"
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_L".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_L")
 
 
 func _on_texture_button_pressed():
-	var parent= get_parent()
+	var parent = get_parent()
 	parent.close()
--- Tims present/NecklaceGame.gd
+++ Tims present/NecklaceGame.gd
@@ -5,21 +5,24 @@ var typed_code = ""
 var entered_code = ""
 var sequence = 0
 
-func _process(_delta): #checks if entered code matches the secret code
+func _process(delta): #checks if entered code matches the secret code
 	if entered_code == secret_code:
-		print("Necklace Correct!")
+		print("Code Correct!")
 		typed_code = ""
 		entered_code = ""
 		var parent = get_parent()
 		if parent and parent.has_method("playercollect"):
 			parent.playercollect()
 			parent.close()
-		else:
-			print("No method necklace")
+	
 	
 
 func _ready():
 	pass
+	
+func wait(seconds: float, letter) -> void:
+	await get_tree().create_timer(seconds).timeout
+	letter.visible = true
 	
 	
 
@@ -36,15 +39,27 @@ func _on_letter_a_button_down():
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_A".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_A")
 func _on_letter_v_button_down():
 	if sequence == 1:
 		typed_code += "V"
 		_append_letter_to_code(typed_code)
 		print(typed_code)
 		sequence += 1
+	$"CanvasLayer/CanvasGroup/letter_V".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_V")
+func _on_letter_i_button_down():
+	$"CanvasLayer/CanvasGroup/letter_I".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_I")
+func _on_letter_e_button_down():
+	$"CanvasLayer/CanvasGroup/letter_E".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_E")
+func _on_letter_s_button_down():
+	$"CanvasLayer/CanvasGroup/letter_S".visible = false
+	wait(1.0, $"CanvasLayer/CanvasGroup/letter_S")
 
 
 func _on_texture_button_pressed():
 	var parent = get_parent()
 	parent.close()
-	
