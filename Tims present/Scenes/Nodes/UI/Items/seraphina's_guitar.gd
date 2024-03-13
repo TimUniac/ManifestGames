82fe65614c83afd881b9361ead493bdc807449aa
Stephilles <149612545+Stephilles@users.noreply.github.com>
2024-03-12 21:02:02 -0400

    hints started highlights added

--- Tims present/Scenes/Nodes/UI/Items/seraphina's_guitar.gd
+++ Tims present/Scenes/Nodes/UI/Items/seraphina's_guitar.gd
@@ -11,6 +11,7 @@ signal guitar_interacted(guitarDialogue)
 var player = null
 var inrange = false
 var is_opened = false
+var hint1 = false
 
 @onready var objlist = $"../Objectives"
 @onready var guitarDialogue: String = "This guitar seems to be important, I wonder if a song would unlock it."
@@ -20,6 +21,9 @@ var gameGuitar = null
 signal chordsSeen
 func _ready():
 	interaction_label.visible = false
+	$"CanvasLayer/Help Box".visible = false
+	$"CanvasLayer/Help Box/Hint1".visible = false
+	$"CanvasLayer/Help Box/Hint2".visible = false
 	
 
 
@@ -42,6 +46,12 @@ func _process(_delta):
 			close()
 		else:
 			open()
+	if is_opened and Input.is_action_just_pressed("Help"):
+		open_hint()
+		if hint1 and Input.is_action_just_pressed("Help"):
+				$"CanvasLayer/Help Box".visible = true
+				$"CanvasLayer/Help Box/Hint1".visible = true
+				$"CanvasLayer/Help Box/Hint2".visible = true
 			
 func open():
 	if gameGuitar == null:
@@ -62,7 +72,21 @@ func close():
 	is_opened = false
 	
 		
- 
+func open_hint():
+	$"CanvasLayer/Help Box".visible = true
+	$"CanvasLayer/Help Box/Hint1".visible = true
+	hint1 = true
+	
+func open_hint2():
+	if hint1 == true:
+		if Input.is_action_just_pressed("Help"):
+			$"CanvasLayer/Help Box".visible = true
+			$"CanvasLayer/Help Box/Hint1".visible = true
+			$"CanvasLayer/Help Box/Hint2".visible = true
+		
+func close_hint():
+	$"CanvasLayer/Help Box".visible = false
+	
 signal item_collected(item_number)
 
 func playercollect():
@@ -78,10 +102,18 @@ func guitar():
 
 signal playGame
 
-
-
-
+func guitar_help():
+	if is_opened and Input.is_action_just_pressed("Help"):
+		$"CanvasLayer/Help Box".visible = true
+		$"CanvasLayer/Help Box/Hint1".visible = true
+		hint1 = true
+		if hint1 and Input.is_action_just_pressed("Help"):
+			$"CanvasLayer/Help Box".visible = true
+			$"CanvasLayer/Help Box/Hint1".visible = true
+			$"CanvasLayer/Help Box/Hint2".visible = true
+		
 func _on_texture_button_pressed():
+	close_hint()
 	close()
 
 func showChords():
