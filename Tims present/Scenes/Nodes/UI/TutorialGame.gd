a2290521c9f568aab76ce8c1fabd79e142b7b459
TimUniac <timuniac@gmail.com>
2024-03-24 00:03:21 -0400

    Bunch of Sounds added. Dialogue Started. Transition sounds added

--- Tims present/Scenes/Nodes/UI/TutorialGame.gd
+++ Tims present/Scenes/Nodes/UI/TutorialGame.gd
@@ -21,32 +21,19 @@ func _ready():
 signal codeCorrect
 
 func _input(event):
+
 	if event is InputEventKey and event.pressed and !event.echo:
+		$Clicks.play_random_pitch()
 		var key = OS.get_keycode_string(event.keycode).capitalize()
 	
-		if key in 'ABCDFGHIJKLMNOPQRSTUVWXYZ1234567890':  
-			var sound_index = _get_sound_index_for_key(key)
-			KeyboardInput.play_sound(sound_index)
+		if key in 'ABCDFGHIJKLMNOPQRSTUVWXYZ1234567890': 
 			_append_letter_to_code(key)
 		elif event.keycode == KEY_ENTER or event.keycode == KEY_KP_ENTER:
 			_on_EnterButton_pressed()
 		elif event.keycode == KEY_BACKSPACE and entered_code != "":
 			entered_code = entered_code.substr(0, entered_code.length() - 1)
 			$CanvasLayer/SequenceInput.text = entered_code
-func _get_sound_index_for_key(key):
-	# Define arrays for alphabet and numbers mapping to their respective indices
-	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	var numbers = "1234567890"
-	var base_index = 0
 
-	if key in alphabet:
-			base_index = alphabet.find(key)
-	elif key in numbers:
-		base_index = 10 + numbers.find(key)
-		if key == '0':  
-			base_index = 25
-
-	return base_index
 func _append_letter_to_code(letter):
 	entered_code += letter
 	$CanvasLayer/SequenceInput.text = entered_code
