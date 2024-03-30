96e2f28d067c2e1d3516d07d7949615d2dfd4c97
TimUniac <70977289+TimUniac@users.noreply.github.com>
2024-03-30 13:38:56 -0400

    Struggle Bus Transit Schedule

--- Tims present/Game_TrainMap.gd
+++ Tims present/Game_TrainMap.gd
@@ -1,23 +1,40 @@
 extends Control
 
+var points = [] # Correct initialization for an empty array
+var correct_sequence = [1, 7, 13, 14, 2] # Assuming IDs for simplicity
+var line2d # Reference to the Line2D node
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Schedule.visible = false
-	#$Map/Area2D.mouse_filter = Control.MOUSE_FILTER_PASS
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	line2d = $Line2D # Adjust the path according to your scene tree
+	for i in range(1, 18):
+		var point_path = "Point" + str(i)
+		var point = get_node(point_path)
+		point.connect("input_event", Callable(self, "_on_Point_input_event"), [i])
 
 func _on_close_pressed():
 	$Schedule.visible = false
 
-
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 		$Schedule.visible = !$Schedule.visible
+
+func _on_Point_input_event(viewport, event, shape_idx, point_id):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var point_node = get_node("Point" + str(point_id))
+		var point_position = point_node.global_position
+		points.append(point_id)
+		line2d.points.append(point_position)
+		
+		if len(points) == correct_sequence.size():
+			if points == correct_sequence:
+				winstate()
+			else:
+				reset()
+
+func winstate():
+	print("Puzzle Solved!")
+
+func reset():
+	line2d.points.clear()
+	points.clear()
