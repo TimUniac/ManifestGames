e733f492a3fd401eb38593e1255c40a10d567b32
Stephilles <149612545+Stephilles@users.noreply.github.com>
2024-04-02 16:03:15 -0400

    Everything is functioning as expected

--- Tims present/Scenes/Levels/ExitBlocker.gd
+++ Tims present/Scenes/Levels/ExitBlocker.gd
@@ -16,3 +16,4 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	if body.has_method("player"):
 		doctor.speak(BlockerDialogue)
+
--- Tims present/script/train_station_scene.gd
+++ Tims present/script/train_station_scene.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 var done = false
+var lightsturnt = false
+var mapdone = false
+var necklacebuilt = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CanvasLayer/Objectives/Label/Orbs.visible = false
 	$CanvasLayer/Objectives/Label4/Orbs.visible = false
 	$CanvasLayer/Objectives/Label2/Orbs.visible = false
 	$CanvasLayer/Objectives/Label3/Orbs.visible = false
+	$ExitBlocker/CollisionShape2D.disabled = false
+	$"ExitBlocker/Area2D/Speech bubble to collect".disabled = false
 	
 	
 	$"Male Doctor Character/Footsteps".activeGround = "parking"
@@ -16,7 +21,7 @@ func _input(_event):
 		hide_show_objectives() 
 
 func _process(delta):
-	pass
+	donegames()
 
 func _on_male_doctor_character_all_items_collected():
 	$ExitBlocker.disabled = true
@@ -32,17 +37,21 @@ func _on_teleport_area_body_entered(body):
 func lightson():
 	$lightgroup.visible = false
 	$CanvasLayer/Objectives/Label/Orbs.visible = true
+	lightsturnt = true
 	
 func mapcompleted():
 	$CanvasLayer/Objectives/Label2/Orbs.visible = true
+	mapdone = true
 	
 func puzzlecomplete():
 	$CanvasLayer/Objectives/Label3/Orbs.visible = true
+	necklacebuilt = true
 
 func hide_show_objectives():
 	if Input.is_action_just_pressed("Objective"):
 		$CanvasLayer/Objectives.visible = !$CanvasLayer/Objectives.visible
 
 func donegames():
-	if done == true:
-		$ExitBlocker.disabled = false
+	if necklacebuilt == true and mapdone ==true and lightsturnt == true :
+		$ExitBlocker/CollisionShape2D.disabled = true
+		$"ExitBlocker/Area2D/Speech bubble to collect".disabled = true
