92e7b2f7e4fb9406d5a36872cc1aaa17711946d5
Stephilles <149612545+Stephilles@users.noreply.github.com>
2024-04-05 01:03:55 -0400

    New Journals Implimented

--- Tims present/Scenes/Levels/ObjectivesTutorial.gd
+++ Tims present/Scenes/Levels/ObjectivesTutorial.gd
@@ -1,13 +1,27 @@
 extends TextureRect
 
+var list = false
+var glasses = false
+var pen= false
+@onready var parent = get_parent().get_node("Finish")
+
+func _ready():
+	parent.visible = false
+func _process(delta):
+	hardyboys()
+	
 func hardyboys():
-	pass
+	if list == true and glasses == true and pen == true:
+		parent.visible = true
 
 func foundlist():
 	$Label2/Orbs2.visible = true
+	list = true
 	
 func foundglasses():
 	$Label/Orbs.visible = true
-	
+	glasses = true
 func foundpen():
 	$Label3/Orbs3.visible = true
+	pen = true
+
